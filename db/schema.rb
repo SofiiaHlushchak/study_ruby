e1139e3225d5b8aaa067eda3b4ca0574783ca684
Sonia <glushchak02@gmail.com>
2021-02-10 10:40:49 +0200

    Finish user signup

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(version: 20210207190632) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "password_digest"
   end
 
