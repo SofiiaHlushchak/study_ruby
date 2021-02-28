f4c7f912ece444ff66ce3e1844f68d8cd63c7ea1
Sonia <glushchak02@gmail.com>
2021-02-28 21:33:38 +0200

    Add user following

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,9 +1,18 @@
 require 'test_helper'
 
-class SessionsControllerTest < ActionController::TestCase
-  test "should get new" do
-    get :new
-    assert_response :success
+class RelationshipsControllerTest < ActionController::TestCase
+
+  test "create should require logged-in user" do
+    assert_no_difference 'Relationship.count' do
+      post :create
+    end
+    assert_redirected_to login_url
   end
 
+  test "destroy should require logged-in user" do
+    assert_no_difference 'Relationship.count' do
+      delete :destroy, id: relationships(:one)
+    end
+    assert_redirected_to login_url
+  end
 end
